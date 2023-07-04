d6316978748337bb6baf0989d998d7f0a3329640
EmirhanKaya <aemirhan@sabanciuniv.edu>
2023-07-04 14:43:00 +0300

    bullet

--- Assets/Scripts/PeopleManager.cs
+++ Assets/Scripts/PeopleManager.cs
@@ -5,11 +5,16 @@ using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
+using UnityEditor.Search;
+
 public class PeopleManager : MonoBehaviour
 {
     public static PeopleManager Instance;
+    public GameObject bullet;
     public int index;
-
+    public float fireRate;
+    public bool canShoot=true;
+    public Animator animator;
     private void Awake()
     {
         if (Instance == null)
@@ -20,12 +25,49 @@ public class PeopleManager : MonoBehaviour
 
     void Start()
     {
+        
         if (SceneManager.GetActiveScene().name == "BossScene")
         {
-            transform.DOMove(BossManager.Instance.transform.position, (3-index)*2f).SetSpeedBased(true);
+            if (index == 0)
+            {
+                transform.DOMove(BossManager.Instance.transform.position, (3-index)*2f).SetSpeedBased(true);
+            }
+
+            if (index == 1)
+            {
+                if (canShoot)
+                {
+                    //GameObject temp = Instantiate(bullet, transform);
+                    //temp.transform.DOMove(BossManager.Instance.transform.position, 1f).SetSpeedBased(true);
+                    //archerShoot();
+                    canShoot = false;
+                    animator.SetTrigger("Shoot");
+                    StartCoroutine(Shoot());
+                }
+            }
+            
         }
     }
 
+    public void archerShoot()
+    {
+        if (canShoot)
+        {
+            GameObject temp = Instantiate(bullet,transform.parent);
+            temp.transform.DOMove(BossManager.Instance.transform.position, 5f).SetSpeedBased(true);
+            canShoot = false;
+            animator.SetTrigger("Shoot");
+            StartCoroutine(Shoot());
+        }
+    }
+    private IEnumerator Shoot()
+    {
+        
+        yield return new WaitForSeconds(fireRate);
+        canShoot = true;
+        animator.SetTrigger("Idle");
+
+    }
     // Update is called once per frame
     void Update()
     {
